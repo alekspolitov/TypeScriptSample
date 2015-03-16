00cb3da4280b421911d96a780d324f882226687e
Aleksey Politov <apolitov@eraser.org>
2015-03-17 00:53:42 +0300

    gulpfile added

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+﻿var gulp = require("gulp"),
+    uglify = require("gulp-uglify"),
+    ts = require("gulp-tsc"),
+    clean = require("gulp-clean");
+
+gulp.task('clean', function () {
+    return gulp.src('_main.js', { read: false })
+      .pipe(clean());
+})
+gulp.task('ts', ["clean"], function () {
+    gulp.src(["**/*.ts", "!node_modules/**/*.ts"])
+      .pipe(ts({
+          out: '_main.js',
+          target: 'ES5',
+          module: 'commonjs'
+      }))
+        .pipe(gulp.dest("."));
+});
+
+gulp.task('default', ['ts']);
